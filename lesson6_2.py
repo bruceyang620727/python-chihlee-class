7cdb72375fb7ef8c5ffcaa3be682991ce0f92445
Bruce Yang <bruceyang620727@gmail.com>
2023-07-08 08:04:23 +0000

    modify

--- lesson6_2.py
+++ lesson6_2.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3.10.2 專案一開始
 
 import random
-#猜數字遊戲
-min = 1
-max = 100
-count = 0
-target = random.randint(min,max)
-print("=============猜數字遊戲==================\n\n")
 
-while True:
-    keyin = int(input(f'猜數字範圍{min}~{max}:'))
-    count += 1
-    if (keyin == target):
-        print(f'賓果!猜對了，答案是:{target}')
-        print(f'你共猜了{count}次')
-        break
-    elif keyin > target:
-        print("再小一點")
-        max = keyin - 1
-    elif keyin < target:
-        print("再大一點")
-        min = keyin + 1
-        print(f"你已經猜了{count}次了!")    
+def playgame():
+    #猜數字遊戲
+    min = 1
+    max = 100
+    count = 0
+    target = random.randint(min,max)
+    print("=============猜數字遊戲==================\n\n")
 
-print("遊戲結束了!下次見")
+    while True:
+        keyin = int(input(f'猜數字範圍{min}~{max}:'))
+        count += 1
+        if (keyin == target):
+            print(f'賓果!猜對了，答案是:{target}')
+            print(f'你共猜了{count}次')
+            break
+        elif keyin > target:
+            print("再小一點")
+            max = keyin - 1
+        elif keyin < target:
+            print("再大一點")
+            min = keyin + 1
+            print(f"你已經猜了{count}次了!")    
+
+    print("遊戲結束了!下次見")
 
 
-
+playgame()
---
+++ lesson6_3.py
@@ -0,0 +1,8 @@
+def func1(part1:int, part2:int):
+    return part1*part2 /2
+
+print(func1(5,10))
+print(func1(7,20))
+print(func1(9,11))
+print(func1(35,90))
+print(func1(34,78))
