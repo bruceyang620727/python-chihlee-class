55ab79712ece7e9157dbeff0d3461497ccd04b68
Bruce Yang <bruceyang620727@gmail.com>
2023-08-12 10:02:02 +0000

    modify

--- lesson13_3.py
+++ lesson13_3.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 value = st.slider("三角函數",min_value=0, max_value=10)
-t = np.arange(0.,value,0.05)
+t = np.arange(0.,5,0.05)
 #print(t)
 #st.write(t)
-y1 = np.sin(2*np.pi*t)
-y2 = np.cos(2*np.pi*t)
+y1 = np.sin(np.random.randn()*value*np.pi*t)
+y1 = np.cos(np.random.randn()*value*np.pi*t)
 #display(y1) #只能用在.ipynb
 #print(y1) #即使改成print，仍舊無法顯示在終端機上
 #display(y2)
