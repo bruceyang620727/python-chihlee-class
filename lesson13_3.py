41154456fc44b90ef5fd68963644d993115663cb
Bruce Yang <bruceyang620727@gmail.com>
2023-08-12 03:14:57 +0000

    modify

--- lesson13_3.py
+++ lesson13_3.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 value = st.slider("三角函數",min_value=0, max_value=10)
-t = np.arange(0,value,0.05)
+t = np.arange(0.,value,0.05)
 #print(t)
 #st.write(t)
 y1 = np.sin(2*np.pi*t)
