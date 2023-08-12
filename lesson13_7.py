1185aa88f0ecff4d98d0e3479de3456e9241f919
Bruce Yang <bruceyang620727@gmail.com>
2023-08-12 14:22:04 +0000

    modify

--- lesson13_7.py
+++ lesson13_7.py
@@ -3,8 +3,8 @@ import streamlit as st
 
 current_weather = pd.read_csv('目前天氣.csv')
 st.write("目前台灣天氣")
-current_weather["啟始時間"] = pd.to_datetime(current_weather["啟始時間"])
-current_weather["啟始時間"] = current_weather["啟始時間"].dt.strftime('%Y-%m-%d-%H:%M:%S')
-current_weather["結束時間"] = pd.to_datetime(current_weather["結束時間"])
-current_weather["結束時間"] = current_weather["結束時間"].dt.strftime('%Y-%m-%d-%H:%M:%S')
+#current_weather["啟始時間"] = pd.to_datetime(current_weather["啟始時間"])
+#current_weather["啟始時間"] = current_weather["啟始時間"].dt.strftime('%Y-%m-%d-%H:%M:%S')
+#current_weather["結束時間"] = pd.to_datetime(current_weather["結束時間"])
+#current_weather["結束時間"] = current_weather["結束時間"].dt.strftime('%Y-%m-%d-%H:%M:%S')
 st.write(current_weather)
